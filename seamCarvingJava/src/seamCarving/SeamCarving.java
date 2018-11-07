d7d05426959cb67659a9956b5dd746275b0b7233
nathan gavin <nathangavin987@gmail.com>
2018-11-08 10:59:16 +1300

    replaced intellij project with eclipse project, also added relevant gitignores

---
+++ seamCarvingJava/src/seamCarving/SeamCarving.java
@@ -0,0 +1,13 @@
+package seamCarving;
+
+import javafx.application.Application;
+import seamCarving.gui.MainWindow;
+
+
+public class SeamCarving {
+
+    public static void main(String[] args) {
+
+        Application.launch(MainWindow.class, args);
+    }
+}
