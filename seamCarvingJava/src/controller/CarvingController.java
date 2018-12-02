e50c175b48ee300f55e84d41ff23d7c0acab63ba
nathan gavin <nathangavin987@gmail.com>
2018-12-02 22:08:40 +1300

    started populating Carving MVC files

---
+++ seamCarvingJava/src/controller/CarvingController.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class CarvingController extends CustomController {
+
+	@Override
+	protected void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- seamCarvingJava/src/model/CarvingModel.java
+++ seamCarvingJava/src/model/CarvingModel.java
@@ -2,18 +2,19 @@ package model;
 
 import java.io.IOException;
 
+import controller.CarvingController;
 import controller.CustomController;
-import controller.ImageSelectController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class ImageSelectModel extends CustomModel {
+public class CarvingModel extends CustomModel {
 
 	@Override
 	public void start(Stage primaryStage) {
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/ImageSelectView.fxml"));
+		
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/CarvingView.fxml"));
 		
 		Parent root = null;
 		try {
@@ -26,8 +27,9 @@ public class ImageSelectModel extends CustomModel {
 		
 		primaryStage.setScene(scene);
 		
-		CustomController controller = fxmlLoader.<ImageSelectController>getController();
+		CustomController controller = fxmlLoader.<CarvingController>getController();
 		controller.init(primaryStage);
+
 	}
 
 }
