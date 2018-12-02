edb134d415e0d55d3654073875bf3445e4c035eb
nathan gavin <nathangavin987@gmail.com>
2018-12-02 22:48:21 +1300

    finished carvingView and began populating carvingController

--- seamCarvingJava/src/controller/CarvingController.java
+++ seamCarvingJava/src/controller/CarvingController.java
@@ -1,7 +1,24 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class CarvingController extends CustomController {
 
+	@FXML private ImageView carvingImageView;
+	@FXML private Label totalSeamsLabel;
+	@FXML private Label currentSeamLabel;
+	@FXML private Button cancelCarvingButton;
+	@FXML private Label horizontalSeamsLabel;
+	@FXML private Label verticalSeamsLabel;
+	@FXML private Label horizontalTotalSeamsLabel;
+	@FXML private Label horizontalCurrentSeamLabel;
+	@FXML private Label verticalCurrentSeamLabel;
+	@FXML private Label verticalTotalSeamsLabel;
+	
 	@Override
 	protected void start() {
 		// TODO Auto-generated method stub
